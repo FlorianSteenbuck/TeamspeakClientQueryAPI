e6f40cbbe68c4d78fc96e83a0bfd92baede99905
Florian Steenbuck <florian.steenbuck@floxen.de>
2015-08-23 16:04:47 +0200

    add help page example

--- examples.php
+++ examples.php
@@ -4,4 +4,13 @@ include("api.php");
 $ts3query = teamspeak_socket_init();
 echo teamspeak_socket_send($ts3query,"clientupdate client_nickname=".teamspeak_escape("I am Stupid"));
 fclose($ts3query);
+
+//Create a Connection and Printout The Help Page
+$ts3query = teamspeak_socket_init();
+$line = teamspeak_socket_send($ts3query,"help");
+while(get_teamspeak_status($line) == null){
+	echo $line;
+	$line = fgets($ts3query);
+}
+fclose($ts3query);
 ?>
